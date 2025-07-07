e07584204d35683fcdda9b0ce8e7b5530c7312d3
Vivekjha-10 <jhavivekkumar81@gmail.com>
2025-07-07 17:46:45 +0530

    .

--- server/api/server.js
+++
@@ -1,4 +0,0 @@
-import app from '../server.js';
-import { createServer } from '@vercel/node';
-
-export default createServer(app);
--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware } from '@clerk/express';
-import { serve } from 'inngest/express';
-import { inngest, functions } from './inngest/index.js';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
 
 const app = express();
+const port = 3000;
 
-app.use(express.json());
-app.use(cors());
-app.use(clerkMiddleware());
+await connectDB()
 
-app.use('/api/inngest', serve({ client: inngest, functions }));
+// Middleware
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
 
-app.get('/', (req, res) => {
-  res.send('Server is Live!');
-});
 
-await connectDB();
+// API Routes
+app.get('/', (req, res) => res.send('Server is Live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
-// 🔥 IMPORTANT: yahan app.listen() mat likhna
-// Instead, export default app
-export default app;
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
