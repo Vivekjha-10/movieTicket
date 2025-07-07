1917a2112ba82356ec37e9488da123af0dbd735b
Vivekjha-10 <jhavivekkumar81@gmail.com>
2025-07-07 17:36:15 +0530

    url fix

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { inngest, functions } from './inngest/index.js';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js";
-
 
 const app = express();
-const port = 3000;
 
-await connectDB()
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
-// Middleware
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
+app.get('/', (req, res) => {
+  res.send('Server is Live!');
+});
 
-// API Routes
-app.get('/', (req, res) => res.send('Server is Live!'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+await connectDB();
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+// 🔥 IMPORTANT: yahan app.listen() mat likhna
+// Instead, export default app
+export default app;
